599d0c78a28ea4805069ae1481b58414c8ae72c8
Denise Draper <draperd@acm.org>
2022-07-31 00:16:23 -0700

    stats bars started; lots of tweaking

--- src/VarViewer.vue
+++ src/VarViewer.vue
@@ -1,8 +1,8 @@
 <script>
-import { prettyprint } from './utility.js';
+import { prettyprint, Stats } from './utility.js';
 
 export default {
-    props: ['datapoints', 'title', 'slidermin', 'slidermax', 'sliderval'],
+    props: ['datapoints', 'title', 'slidermin', 'slidermax', 'initial_value'],
 
     data() {
         return {
@@ -11,6 +11,7 @@ export default {
             maxval: 1,
             range: 1,
             value: 1,
+            show_explore: false,
             show_details: false,
             active_tab: 0,
             last_item: null
@@ -23,12 +24,29 @@ export default {
     },
 
     computed: {
-        slider_pos() {
-            return (this.value - this.minval) / this.range;
+        selected_points() {
+            return this.realdp.filter(x => x.enabled);
         },
+        selected_values() {
+            return this.selected_points.map(x => x.value);
+        },
+
+        // the bar positions we (might) show on the chart
+        minbar() {
+            return Math.min(...this.selected_values);
+        },
+        maxbar() {
+            return Math.max(...this.selected_values);
+        },
+        medianbar() {
+            return Stats.median(this.selected_values);
+        },
+        quartiles() {
+            return Stats.quartiles(this.selected_values);
+        }
     },
     methods: {
-        prettyprint,
+
         initialize() {
             this.minval = this.slidermin
                 ? this.slidermin
@@ -38,16 +56,12 @@ export default {
                 : 1.1 * Math.max(...this.realdp.map((x) => x.value));
             this.range = this.maxval - this.minval;
             for (let i in this.realdp) {
-                this.realdp[i].xpos =
-                    ((100 * this.realdp[i].value) / this.range).toString() + '%';
+                this.realdp[i].xpos = (100 * (this.realdp[i].value - this.minval) / this.range).toString() + '%';
             }
-            this.value = this.sliderval
-                ? this.sliderval
-                : this.minval + this.range / 2;
-        },
-        update_value(e) {
-            const newval = e.target.value * this.range + this.minval;
-            this.value = newval;
+            this.value = this.initial_value
+                ? this.initial_value
+                : this.minval + this.maxval / 2;
+            console.log(this);
         },
         toggle_details() {
             this.show_details = !this.show_details;
@@ -64,6 +78,14 @@ export default {
         highlight_list_item(item) {
             item.highlight = true;
             this.last_item = item;
+        },
+
+        // These are utilities that are used to construct values that affect rendering.
+        // Hence they end up as methods, even though that feels a bit weird.
+        prettyprint,
+        // Calculate the x position for the chart, expressed as a %
+        val_to_xpos(v) {
+            return (100 * (v - this.minval) / this.range).toString() + '%';
         }
     },
 };
@@ -71,87 +93,105 @@ export default {
 <!-- TEMPLATE -->
 <template>
     <div class="container max-w-sm mx-auto mt-5">
-        <h1 class="text-lg text-center">
-            {{ title }}
-        </h1>
-        <h2 class="text-sm text-center leading-none" v-if="realdp.length">
-            {{ realdp[0].units }}
-        </h2>
-        <!-- BEGIN CHART -->
-        <div id="chart" class="container relative h-32 bg-zinc-100 cursor-pointer">
-            <!-- points -->
-            <div v-for="item of realdp" class="point absolute bottom-[14px]" :class="{
-                'text-blue-500': item.highlight,
-                'text-zinc-400': !item.enabled,
-            }" :style="{ left: item.xpos }" @pointerenter="item.highlight = true" @pointerleave="item.highlight = false">
-                🟂
-            </div>
-            <!-- slider -->
-            <input type="range" id="slider" min="0" max="1" step="any" :value="slider_pos" @input="update_value" />
+        <div class="w-max m-auto">
+            <h1 class="text-lg text-center">
+                {{ title }}
+            </h1>
+            <h2 class="text-sm text-center leading-none" v-if="realdp.length">
+                {{ realdp[0].units }}
+            </h2>
             <!-- value -->
-            <div class="absolute bottom-0.5 left-1 text-xs">
-                {{ prettyprint(value) }}
+            <div class="flex justify-center m-auto my-2 ring-offset-2 ring-2">
+                <div class="grow"> {{ prettyprint(value) }} </div>
+                <div class="flex-none ml-1.5 hover:bg-slate-400"
+                    @click="show_explore=true"><i class="fa-solid fa-pen-to-square"></i></div>
             </div>
         </div>
-        <!-- BEGIN DETAILS -->
-        <div class="w-fit text-sm p-1.5 pr-2 hover:bg-zinc-400" @click="toggle_details()">
-            {{ show_details ? 'hide' : 'show' }} sources
-        </div>
-        <!-- why isn't v-if working?  It shows when clicked, but does not hide. 
+
+
+        <div id="explore" v-if="show_explore">
+            <!-- BEGIN CHART -->
+            <div id="chart" class="container relative h-32 bg-zinc-100 cursor-pointer">
+                <!-- points -->
+                <div v-for="item of realdp" class="point absolute bottom-[14px]" :class="{
+                    'text-blue-500': item.highlight,
+                    'text-zinc-400': !item.enabled,
+                }" :style="{ left: item.xpos }" @pointerenter="item.highlight = true"
+                    @pointerleave="item.highlight = false">
+                    🟂
+                </div>
+
+                <!-- bars -->
+                <div class="vv-vbar" :style="{ left: val_to_xpos(minbar) }"><b>min</b> {{ prettyprint(minbar) }}</div>
+                <!-- <div class="vv-vbar" :style="{left : val_to_xpos(quartiles[0])}"><b>1Q</b> {{prettyprint(quartiles[0])}}</div>
+            <div class="vv-vbar" :style="{left : val_to_xpos(medianbar)}"><b>median</b> {{prettyprint(medianbar)}}</div>
+            <div class="vv-vbar" :style="{left : val_to_xpos(quartiles[2])}"><b>3Q</b> {{prettyprint(quartiles[2])}}</div> -->
+                <div class="vv-vbar" :style="{ left: val_to_xpos(maxbar) }"><b>max</b> {{ prettyprint(maxbar) }}</div>
+                <!-- slider -->
+                <input type="range" id="slider" :min="minval" :max="maxval" step="any" v-model="value"
+                    class="accent-blue-500" />
+            </div>
+            <!-- BEGIN DETAILS -->
+            <div class="w-fit text-sm p-1.5 pr-2 hover:bg-zinc-400" @click="toggle_details()">
+                {{ show_details ? 'hide' : 'show' }} sources
+            </div>
+            <!-- why isn't v-if working?  It shows when clicked, but does not hide. 
          same thing happens if v-if is on the o-tabs component directly.
          v-show works though, so we'll stick with that. -->
-        <div id="details-container" v-show="show_details" class="cursor-default">
-            <!-- Oruga styling is in ../index.css -->
-            <o-tabs v-model="active_tab" :animated="false" ref="tabs">
-                <!-- Tab 1: the list of entries -->
-                <o-tab-item value="0" label="List">
-                    <ul>
-                        <li v-for="item of realdp" @pointerenter="highlight_list_item(item)"
-                            @pointerleave="item.highlight = false" class="text-sm ml-2" :class="{
-                                'text-blue-500': item.highlight,
-                                'text-zinc-400': !item.enabled,                                
-                            }">
-                            <input type="checkbox" :checked="item.enabled"
-                                @click="item.enabled = !item.enabled" :class="{
-                                    'accent-blue-500': item.highlight,
-                                    'accent-zinc-400': !item.enabled,  
-                                }"/>&nbsp;
-                            {{ item.source }} ({{ item.category }}): {{ prettyprint(item.value) }}
-                            <!-- Show more details when highlighted -->
-                            <Teleport to="#pane">
-                                <div v-if="item == last_item">
-                                    {{ item.citation }}
-                                    <span v-if="item.link">
-                                        <a :href="item.link" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
-                                    </span>
-                                    <br/>
-                                    {{ prettyprint(item.original_value) }} {{ item.original_units }}
-                                    <span v-if="item.original_value != item.value"> = 
-                                        {{ prettyprint(item.value) }} {{ item.units }}
-                                    </span>
-                                </div>
-                            </Teleport>
-                        </li>
-                    </ul>
-                    <div id="pane" class="w-full h-36 bg-zinc-100 py-1 px-2 text-sm"></div>
-                </o-tab-item>
-                <!-- Tab 2: selection by source-->
-                <o-tab-item :value="1" label="Select by Source">
-                    Lorem <br />
-                    ipsum <br />
-                    dolor <br />
-                    sit <br />
-                    amet.
-                </o-tab-item>
-                <!-- Tab 3: se3lection by category -->
-                <o-tab-item :value="2" label="Select by Category">
-                    Lorem <br />
-                    ipsum <br />
-                    dolor <br />
-                    sit <br />
-                    amet.
-                </o-tab-item>
-            </o-tabs>
+            <div id="details-container" v-show="show_details" class="cursor-default">
+                <!-- Oruga styling is in ../index.css -->
+                <o-tabs v-model="active_tab" :animated="false" ref="tabs">
+                    <!-- Tab 1: the list of entries -->
+                    <o-tab-item value="0" label="List">
+                        <ul>
+                            <li v-for="item of realdp" @pointerenter="highlight_list_item(item)"
+                                @pointerleave="item.highlight = false" class="text-sm ml-2" :class="{
+                                    'text-blue-500': item.highlight,
+                                    'text-zinc-400': !item.enabled,
+                                }">
+                                <input type="checkbox" :checked="item.enabled" @click="item.enabled = !item.enabled"
+                                    :class="{
+                                        'accent-blue-500': item.highlight,
+                                        'accent-zinc-400': !item.enabled,
+                                    }" />&nbsp;
+                                {{ item.source }} ({{ item.category }}): {{ prettyprint(item.value) }}
+                                <!-- Show more details when highlighted -->
+                                <Teleport to="#pane">
+                                    <div v-if="item == last_item">
+                                        {{ item.citation }}
+                                        <span v-if="item.link">
+                                            <a :href="item.link" target="_blank"><i
+                                                    class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                                        </span>
+                                        <br />
+                                        {{ prettyprint(item.original_value) }} {{ item.original_units }}
+                                        <span v-if="item.original_value != item.value"> =
+                                            {{ prettyprint(item.value) }} {{ item.units }}
+                                        </span>
+                                    </div>
+                                </Teleport>
+                            </li>
+                        </ul>
+                        <div id="pane" class="w-full h-36 bg-zinc-100 py-1 px-2 text-sm"></div>
+                    </o-tab-item>
+                    <!-- Tab 2: selection by source-->
+                    <o-tab-item :value="1" label="Select by Source">
+                        Lorem <br />
+                        ipsum <br />
+                        dolor <br />
+                        sit <br />
+                        amet.
+                    </o-tab-item>
+                    <!-- Tab 3: se3lection by category -->
+                    <o-tab-item :value="2" label="Select by Category">
+                        Lorem <br />
+                        ipsum <br />
+                        dolor <br />
+                        sit <br />
+                        amet.
+                    </o-tab-item>
+                </o-tabs>
+            </div>
         </div>
     </div>
 </template>
@@ -164,5 +204,18 @@ export default {
     position: absolute;
     bottom: 34px;
     height: 2px;
+}
+
+.vv-vbar {
+    position: absolute;
+    bottom: 35px;
+    width: 80px;
+    height: 4px;
+    font-size: 60%;
+    color: red;
+    border-top: 1px red;
+
+    transform: rotate(-90deg);
+    transform-origin: top left;
 }
 </style>
--- src/utility.js
+++ src/utility.js
@@ -9,3 +9,39 @@ export function prettyprint(n) {
   // Pick a representation for number that is most compact.
   return n > 0.01 ? bignum.format(n) : smallnum.format(n).replace('E', 'e');
 }
+
+// Static class exposes statistical methods
+// h/t to https://gist.github.com/Daniel-Hug/7273430
+
+// Note! some of these functions use sort, which modifies arrays in place.
+// If that is not acceptable, copy the data first.
+
+export class Stats {
+
+    static median(array) {
+        if (!array.length) return null;
+        array.sort((a, b)=>a-b);
+		const mid = array.length / 2;
+		return mid % 1 ? array[mid - 0.5] : (array[mid - 1] + array[mid]) / 2;
+    }
+
+    static quartiles(array) {
+        if(array.length < 4) return null;
+        array.sort((a,b)=>a-b);
+        const a = array;
+        const n = a.length;
+        const mp = (i) => (array[i] + array[i+1])/2;
+
+        // There are several ways to do quartiles; this one excludes the midpoint from the 25% and 75% measures
+        // in the case of an odd number of datapoints.
+        if ( n%2 == 0 ) {
+            const mi = n/2;
+            return [ Stats.median(a.slice(0,mi)), mp(mi), Stats.median(a.slice(mi))];
+        }
+        else {
+            const mi = Math.floor(n/2);
+            return [ Stats.median(a.slice(0,mi)), a[mi], Stats.median(a.slice(mi+1))];
+        }
+    }
+}
+
